fc7044351f791977fec653b01d26b94f8aa1dd59
Roman Pavlov <pra_dev@mail.ru>
2022-04-09 20:52:55 +0500

    Lesson 2: Models

--- sites/news/models.py
+++ sites/news/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
-# Create your models here.
 
+class News(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField()
+    views = models.IntegerField()
+    # image
+    # comments
+    # category
+
+# todo Create new model for Commentary
+#  create category model
