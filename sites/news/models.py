3b5cc051b3b90feef63dfc62d9e5d7437ecf8b7b
AlexsandrBelkin <sahsabelkin50821@gmai.com>
2022-04-16 18:41:58 +0300

    создал класс комментария и сделал миграцию

--- sites/news/models.py
+++ sites/news/models.py
@@ -1,16 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    content = models.CharField(max_length=500)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField()
     views = models.IntegerField()
     # image
-    # comments
-    # category
+# todo create category model
 
-# todo Create new model for Commentary
-#  create category model
+
+class Commentary(models.Model):
+    ip = models.CharField(max_length=15)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    text = models.TextField()
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
